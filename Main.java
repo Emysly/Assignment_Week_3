1ee83479579f37e73dd090feeabcb080483c7b4b
emysly <emysilva@users-MacBook-Pro.local>
2020-08-02 18:06:56 +0100

    refactoring

--- Main.java
+++ Main.java
@@ -1,64 +1,80 @@
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	static Map<String, String> map = new LinkedHashMap<>();
 
-		Properties prodProps = new Properties();
-		Properties devProps = new Properties();
-		Properties stagingProps = new Properties();
+	public static void configEnv(String config) throws FileNotFoundException {
+		ConfigParser configEnv = new ConfigParser(config);
 
-		//passed prod, dev and staging file to the configParser
-		ConfigParser prodConfig = new ConfigParser("config.txt");
-		ConfigParser devConfig = new ConfigParser("config.txt.dev");
-		ConfigParser stagingConfig = new ConfigParser("config.txt.staging");
+		// read development config file
+		File f1 = new File(configEnv.getConfig());
+		Scanner devIn = new Scanner(f1);
+
+		//checks if there is next line of characters and split where it
+		//founds '=' to have two words which forms an array and it also
+		// checks that the array is atleast 2 words, finally store it
+		// in a map if the key does not exist in the map
+		while(devIn.hasNextLine()) {
+			String[] arr = devIn.next().split("=");
+			if(arr.length >= 2) map.putIfAbsent(arr[0], arr[1]);
+		}
+
+
+	}
+	public static void main(String[] args) {
+
 
 		//read prod, dev and staging files
-		try (FileInputStream prod = new FileInputStream(prodConfig.getConfig());
-			 FileInputStream dev = new FileInputStream(devConfig.getConfig());
-			 FileInputStream staging = new FileInputStream(stagingConfig.getConfig())
-		) {
-
-
-			//loads the prod file
-		    prodProps.load(prod);
-
-			//loads the dev file
-			devProps.load(dev);
-
-			//loads the staging file
-			stagingProps.load(staging);
-
-
-			Map<String, String> env = new LinkedHashMap<>();
-
-			if (args.length != 0) {
-
+		try {
+			if(args.length != 0) {
 				//scans the input from command line args
 				switch (args[0]) {
-					case "production": {
-						env.put("dbName", prodProps.get("dbname").toString());
-						env.put("productionDbname", prodProps.get("name").toString());
-						System.out.println(env);
-						break;
-					}
+
 					case "development": {
-						env.put("dbName", devProps.get("dbname").toString());
-						env.put("devDbname", devProps.get("name").toString());
-						System.out.println(env);
+
+						configEnv("config.txt.dev");
+
+						//use the second map to map the properties of the config file that is stored in the first map
+						Map<String, String> devEnv = new LinkedHashMap<>();
+
+						devEnv.put("dbName", map.get("dbname"));
+						devEnv.put("developmentDbname", map.get("name"));
+						System.out.println(devEnv);
 						break;
 					}
 					case "staging": {
-						env.put("dbName", stagingProps.get("dbname").toString());
-						env.put("stagingDbname", stagingProps.get("name").toString());
-						System.out.println(env);
+						configEnv("config.txt.staging");
+
+						//use the second map to map the properties of the config file
+						// that is stored in the first map
+						Map<String, String> stagingEnv = new LinkedHashMap<>();
+
+						stagingEnv.put("dbName", map.get("dbname"));
+						stagingEnv.put("stagingDbname", map.get("name"));
+						System.out.println(stagingEnv);
 						break;
 					}
 				}
+
+			}
+			else{
+				configEnv("config.txt");
+
+				//use the second map to map the properties of the config file that is stored in the first map
+				Map<String, String> prodEnv = new LinkedHashMap<>();
+
+				prodEnv.put("dbName", map.get("dbname"));
+				prodEnv.put("productionDbname", map.get("name"));
+				System.out.println(prodEnv);
 			}
 
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
