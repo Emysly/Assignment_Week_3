bda6eb54fd89be977a4d348e76ba411d64baace5
emysly <emysilva@users-MacBook-Pro.local>
2020-08-04 00:55:29 +0100

    refactoring

--- ConfigParser.java
+++ ConfigParser.java
@@ -1,7 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class ConfigParser {
 
 	private String config;
+
+	private Map<String, String> env = new LinkedHashMap<>();
 
 	public ConfigParser(String config) {
 		this.config = config;
@@ -19,10 +26,57 @@ public class ConfigParser {
 		this.config = config;
 	}
 
+	public Map<String, String> getEnv() {
+		return env;
+	}
+
 	@Override
 	public String toString() {
 		return "ConfigParser{" +
 				"config='" + config + '\'' +
 				'}';
+	}
+
+	public void configEnv(String fileName) throws FileNotFoundException {
+		ConfigParser config = new ConfigParser(fileName);
+
+		// read development config file
+		File configFile = new File(config.getConfig());
+		Scanner scanner = new Scanner(configFile);
+
+		String[] arr;
+		String prefix = "";
+
+		//checks if there is next line of characters and split where it
+		//founds '=' character to have two words which forms an array and it also
+		// checks that the array is at least 2 words, finally store it
+		// in a map if the key does not exist in the map
+		while(scanner.hasNextLine()) {
+			arr = scanner.next().split("=");
+			if(arr.length > 1)
+				//puts if the key doesn't already exist in the map
+				config.getEnv().putIfAbsent(prefix + arr[0], arr[1]);
+
+
+			//checks if the first item in the array has '[' character
+			if (arr[0].contains("[")) {
+				prefix = arr[0].substring(1, arr[0].length() - 1);
+				prefix += ".";
+			}
+			//if there is nothing in the array
+			if (arr[0].length() == 0) prefix = "";
+		}
+
+		System.out.println("You are currently in the " + config.get("application.mode") + " mode...");
+		//gets the values of the config map
+		String dbName = config.get("dbname");
+		String environmentDbname = config.get("application.name");
+		System.out.println(dbName);
+		System.out.println(environmentDbname);
+
+	}
+
+	public String get(String key) {
+		return env.get(key);
 	}
 }
--- Main.java
+++ Main.java
@@ -1,48 +1,10 @@
 import java.io.*;
-import java.util.*;
 
-public class Main {
+public class Main extends ConfigParser {
 
-	static Map<String, String> config = new LinkedHashMap<>();
-
-	public static void configEnv(String fileName) throws FileNotFoundException {
-		ConfigParser configEnv = new ConfigParser(fileName);
-
-		// read development config file
-		File configFile = new File(configEnv.getConfig());
-		Scanner scanner = new Scanner(configFile);
-
-		String[] arr;
-		String prefix = "";
-
-		//checks if there is next line of characters and split where it
-		//founds '=' character to have two words which forms an array and it also
-		// checks that the array is at least 2 words, finally store it
-		// in a map if the key does not exist in the map
-		while(scanner.hasNextLine()) {
-			arr = scanner.next().split("=");
-			if(arr.length > 1) {
-
-				//puts if the key doesn't already exist in the map
-				config.putIfAbsent(prefix + arr[0], arr[1]);
-				}
-
-			//checks if the first item in the array has '[' character
-			if (arr[0].contains("[")) {
-				prefix = arr[0].substring(1, arr[0].length() - 1);
-				prefix += ".";
-			}
-			//if there is nothing in the array
-			if (arr[0].length() == 0) prefix = "";
-		}
-
-		//gets the values of the config map
-		String dbName = config.get("dbname");
-		String environmentDbname = config.get("application.name");
-		System.out.println(dbName);
-		System.out.println(environmentDbname);
-	}
 	public static void main(String[] args) {
+
+		ConfigParser config = new ConfigParser();
 
 		try {
 			//checks if an arg is provided from the terminal
@@ -51,16 +13,16 @@ public class Main {
 				switch (args[0].toLowerCase()) {
 					case "development" ->
 						//if arg is development, pass the development file
-						configEnv("config.txt.dev");
+						config.configEnv("config.txt.dev");
 
 					case "staging" ->
 						//if arg is staging, pass the staging file
-						configEnv("config.txt.staging");
+						config.configEnv("config.txt.staging");
 				}
 			}
 			else{
 				//without any arg pass the production file
-				configEnv("config.txt");
+				config.configEnv("config.txt");
 			}
 
 		} catch (IOException e) {
