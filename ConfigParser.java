f091d3f787f64d00643b368998866f8022c34e0e
emysly <emysilva@users-MacBook-Pro.local>
2020-08-04 19:56:54 +0100

    refactoring

--- ConfigParser.java
+++ ConfigParser.java
@@ -1,87 +1,30 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ConfigParser {
 
-	private String config;
+	private String filename;
 
 	private final Map<String, String> env = new LinkedHashMap<>();
 
 	public ConfigParser(String config) {
-		this.config = config;
+		this.filename = config;
 	}
 
 	public ConfigParser() {
-		this.config = "default-name";
+		this.filename = "default-name";
 	}
 
-	public String getConfig() {
-		return config;
+	public String getFilename() {
+		return filename;
 	}
 
-	public void setConfig(String config) {
-		this.config = config;
+	public void setFilename(String filename) {
+		this.filename = filename;
 	}
 
 	public Map<String, String> getEnv() {
 		return env;
-	}
-
-	@Override
-	public String toString() {
-		return "ConfigParser{" +
-				"config='" + config + '\'' +
-				'}';
-	}
-
-	//a method that handles the environment implementations
-	public void configEnv(String fileName) throws IOException {
-
-		final ConfigParser config = new ConfigParser(fileName);
-
-		//get the file path
-		final Path path = Paths.get(config.getConfig());
-
-		//add all lines to an array
-		final List<String> strings = Files.readAllLines(path, StandardCharsets.UTF_8);
-
-		String[] arr;
-		String prefix = "";
-
-		//loop through the array to print each line of string
-		for (String string : strings) {
-			arr = string.split("=");
-
-			if (arr.length > 1)
-				//puts if the key doesn't already exist in the map
-				config.getEnv().putIfAbsent(prefix + arr[0], arr[1]);
-
-
-			//checks if the first item in the array has '[' character
-			if (arr[0].contains("[")) {
-				prefix = arr[0].substring(1, arr[0].length() - 1);
-				prefix += ".";
-			}
-
-			//if there is nothing in the array
-			if (arr[0].length() == 0) prefix = "";
-		}
-
-		System.out.println("You are currently in the " + config.get("mode") + " mode...");
-
-		//gets the values of the config map
-		final String dbName = config.get("dbname");
-		final String environmentDbname = config.get("application.name");
-		System.out.println(dbName);
-		System.out.println(environmentDbname);
 	}
 
 	public String get(String key) {
--- Main.java
+++ Main.java
@@ -1,10 +1,57 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main extends ConfigParser {
 
-	public static void main(String[] args) {
+	//a method that handles the environment implementations
+	public static void configEnv(String fileName, String mode) throws IOException {
 
-		ConfigParser config = new ConfigParser();
+		fileName = fileName.replace(".txt", mode);
+		//pass the file to the configparser
+		final ConfigParser config = new ConfigParser(fileName);
+
+		//get the file path
+		final Path path = Paths.get(config.getFilename());
+
+		//add all lines to an array
+		final List<String> strings = Files.readAllLines(path, StandardCharsets.UTF_8);
+
+		String[] arr;
+		String prefix = "";
+
+		//loop through the array to print each line of string
+		for (String string : strings) {
+			arr = string.split("=");
+
+			if (arr.length > 1)
+				//puts if the key doesn't already exist in the map
+				config.getEnv().putIfAbsent(prefix + arr[0], arr[1]);
+
+
+			//checks if the first item in the array has '[' character
+			if (arr[0].contains("[")) {
+				prefix = arr[0].substring(1, arr[0].length() - 1);
+				prefix += ".";
+			}
+
+			//if there is nothing in the array
+			if (arr[0].length() == 0) prefix = "";
+		}
+
+		System.out.println("You are currently in the " + config.get("mode") + " mode...");
+
+		//gets the values of the config map
+		final String dbName = config.get("dbname");
+		final String environmentDbname = config.get("application.name");
+		System.out.println(dbName);
+		System.out.println(environmentDbname);
+	}
+
+	public static void main(String[] args) {
 
 		try {
 
@@ -17,19 +64,19 @@ public class Main extends ConfigParser {
 					case "development" ->
 
 						//if arg is development, pass the development file
-						config.configEnv("config.txt.dev");
+						configEnv("config.txt", "-dev.txt");
 
 					case "staging" ->
 
 						//if arg is staging, pass the staging file
-						config.configEnv("config.txt.staging");
+						configEnv("config.txt", "-staging.txt");
 
 				}
 			}
 			else{
 
 				//without any arg pass the production file
-				config.configEnv("config.txt");
+				configEnv("config.txt", ".txt");
 
 			}
 
